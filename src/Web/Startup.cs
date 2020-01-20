991de5d426e9908ab959d36cb5bf31f8343baf7b
Fernando Almeida <nanditu@gmail.com>
2020-01-20 12:00:20 +0000

    Revised Startup to load Azure-environment specific services. Added new launch profile for Azure environment.

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -1,12 +1,18 @@
-﻿using Ardalis.ListStartupServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+
+using Ardalis.ListStartupServices;
+
 using MediatR;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Services;
 using Microsoft.eShopWeb.Infrastructure.Data;
@@ -15,39 +21,33 @@ using Microsoft.eShopWeb.Infrastructure.Logging;
 using Microsoft.eShopWeb.Infrastructure.Services;
 using Microsoft.eShopWeb.Web.Interfaces;
 using Microsoft.eShopWeb.Web.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mime;
 
-namespace Microsoft.eShopWeb.Web
-{
-    public class Startup
-    {
+using Newtonsoft.Json;
+
+namespace Microsoft.eShopWeb.Web {
+    public class Startup {
         private IServiceCollection _services;
-        public Startup(IConfiguration configuration)
-        {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment) {
             Configuration = configuration;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureDevelopmentServices(IServiceCollection services)
-        {
+        public void ConfigureDevelopmentServices(IServiceCollection services) {
             // use in-memory database
             ConfigureInMemoryDatabases(services);
-
             // use real database
-            //ConfigureProductionServices(services);
         }
 
-        private void ConfigureInMemoryDatabases(IServiceCollection services)
-        {
+        private void ConfigureInMemoryDatabases(IServiceCollection services) {
             // use in-memory database
             services.AddDbContext<CatalogContext>(c =>
                 c.UseInMemoryDatabase("Catalog"));
@@ -59,13 +59,12 @@ namespace Microsoft.eShopWeb.Web
             ConfigureServices(services);
         }
 
-        public void ConfigureProductionServices(IServiceCollection services)
-        {
+        public void ConfigureProductionServices(IServiceCollection services) {
             // use real database
             // Requires LocalDB which can be installed with SQL Server Express 2016
             // https://www.microsoft.com/en-us/download/details.aspx?id=54284
-            services.AddDbContext<CatalogContext>(c =>
-                c.UseSqlServer(Configuration.GetConnectionString("CatalogConnection")));
+            services.AddDbContext<CatalogContext>(dbContextOptionsBuilder =>
+                dbContextOptionsBuilder.UseSqlServer(Configuration.GetConnectionString("CatalogConnection")));
 
             // Add Identity DbContext
             services.AddDbContext<AppIdentityDbContext>(options =>
@@ -73,16 +72,16 @@ namespace Microsoft.eShopWeb.Web
 
             ConfigureServices(services);
         }
+        public void ConfigureAzureServices(IServiceCollection services) {
+            ConfigureProductionServices(services);
+        }
 
-        public void ConfigureTestingServices(IServiceCollection services)
-        {
+        public void ConfigureTestingServices(IServiceCollection services) {
             ConfigureInMemoryDatabases(services);
         }
 
-
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             ConfigureCookieSettings(services);
 
             CreateIdentityIfNotCreated(services);
@@ -105,33 +104,29 @@ namespace Microsoft.eShopWeb.Web
             // Add memory cache services
             services.AddMemoryCache();
 
-            services.AddRouting(options =>
-            {
+            services.AddRouting(options => {
                 // Replace the type and the name used to refer to it with your own
                 // IOutboundParameterTransformer implementation
                 options.ConstraintMap["slugify"] = typeof(SlugifyParameterTransformer);
             });
 
-            services.AddMvc(options =>
-            {
+            services.AddMvc(options => {
                 options.Conventions.Add(new RouteTokenTransformerConvention(
-                         new SlugifyParameterTransformer()));
+                    new SlugifyParameterTransformer()));
 
-            });    
-            services.AddRazorPages(options =>
-            {
+            });
+            services.AddRazorPages(options => {
                 options.Conventions.AuthorizePage("/Basket/Checkout");
             });
             services.AddControllersWithViews();
 
             services.AddHttpContextAccessor();
-            
-            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1"}));
+
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" }));
 
             services.AddHealthChecks();
 
-            services.Configure<ServiceConfig>(config =>
-            {
+            services.Configure<ServiceConfig>(config => {
                 config.Services = new List<ServiceDescriptor>(services);
 
                 config.Path = "/allservices";
@@ -140,74 +135,59 @@ namespace Microsoft.eShopWeb.Web
             _services = services; // used to debug registered services
         }
 
-        private static void CreateIdentityIfNotCreated(IServiceCollection services)
-        {
+        private static void CreateIdentityIfNotCreated(IServiceCollection services) {
             var sp = services.BuildServiceProvider();
-            using (var scope = sp.CreateScope())
-            {
+            using(var scope = sp.CreateScope()) {
                 var existingUserManager = scope.ServiceProvider
                     .GetService<UserManager<ApplicationUser>>();
-                if(existingUserManager == null)
-                {
+                if (existingUserManager == null) {
                     services.AddIdentity<ApplicationUser, IdentityRole>()
                         .AddDefaultUI()
                         .AddEntityFrameworkStores<AppIdentityDbContext>()
-                                        .AddDefaultTokenProviders();
+                        .AddDefaultTokenProviders();
                 }
             }
         }
 
-        private static void ConfigureCookieSettings(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
+        private static void ConfigureCookieSettings(IServiceCollection services) {
+            services.Configure<CookiePolicyOptions>(options => {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-            services.ConfigureApplicationCookie(options =>
-            {
+            services.ConfigureApplicationCookie(options => {
                 options.Cookie.HttpOnly = true;
                 options.ExpireTimeSpan = TimeSpan.FromHours(1);
                 options.LoginPath = "/Account/Login";
                 options.LogoutPath = "/Account/Logout";
-                options.Cookie = new CookieBuilder
-                {
+                options.Cookie = new CookieBuilder {
                     IsEssential = true // required for auth to work without explicit user consent; adjust to suit your privacy policy
                 };
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             app.UseHealthChecks("/health",
-                new HealthCheckOptions
-                {
-                    ResponseWriter = async (context, report) =>
-                    {
+                new HealthCheckOptions {
+                    ResponseWriter = async(context, report) => {
                         var result = JsonConvert.SerializeObject(
-                            new
-                            {
+                            new {
                                 status = report.Status.ToString(),
-                                errors = report.Entries.Select(e => new
-                                {
-                                    key = e.Key,
-                                    value = Enum.GetName(typeof(HealthStatus), e.Value.Status)
-                                })
+                                    errors = report.Entries.Select(e => new {
+                                        key = e.Key,
+                                            value = Enum.GetName(typeof(HealthStatus), e.Value.Status)
+                                    })
                             });
                         context.Response.ContentType = MediaTypeNames.Application.Json;
                         await context.Response.WriteAsync(result);
                     }
                 });
-            if (env.IsDevelopment())
-            {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
                 app.UseShowAllServicesMiddleware();
                 app.UseDatabaseErrorPage();
-            }
-            else
-            {
+            } else {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
@@ -215,7 +195,7 @@ namespace Microsoft.eShopWeb.Web
 
             app.UseStaticFiles();
             app.UseRouting();
-            
+
             app.UseHttpsRedirection();
             app.UseCookiePolicy();
             app.UseAuthentication();
@@ -225,13 +205,11 @@ namespace Microsoft.eShopWeb.Web
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
             // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
+            app.UseSwaggerUI(c => {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
 
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseEndpoints(endpoints => {
                 endpoints.MapControllerRoute("default", "{controller:slugify=Home}/{action:slugify=Index}/{id?}");
                 endpoints.MapRazorPages();
                 endpoints.MapHealthChecks("home_page_health_check");
