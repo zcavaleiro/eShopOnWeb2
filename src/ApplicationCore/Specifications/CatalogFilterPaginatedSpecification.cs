30a25249feb474d49fe91937c80d52a29dde20ae
Fernando Almeida <nanditu@gmail.com>
2020-01-31 11:12:22 +0000

    Fix linting issue (obsolete method) Revised catalog filter spec to consider case-insensitive comparison. Refactor build catalog filter to a common place using inheritance.

--- src/ApplicationCore/Specifications/CatalogFilterPaginatedSpecification.cs
+++ src/ApplicationCore/Specifications/CatalogFilterPaginatedSpecification.cs
@@ -2,15 +2,12 @@
 
 namespace Microsoft.eShopWeb.ApplicationCore.Specifications
 {
-    public class CatalogFilterPaginatedSpecification : BaseSpecification<CatalogItem>
+    public class CatalogFilterPaginatedSpecification : CatalogFilterSpecification
     {
         public CatalogFilterPaginatedSpecification(
             int skip, int take,
             string searchText, int? brandId, int? typeId)
-            : base(i =>
-                (!brandId.HasValue || i.CatalogBrandId == brandId) &&
-                (!typeId.HasValue || i.CatalogTypeId == typeId) &&
-                (string.IsNullOrEmpty(searchText) || i.Name.Contains(searchText)))
+            : base(searchText, brandId, typeId)
         {
             ApplyPaging(skip, take);
         }
--- src/ApplicationCore/Specifications/CatalogFilterSpecification.cs
+++ src/ApplicationCore/Specifications/CatalogFilterSpecification.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.eShopWeb.ApplicationCore.Entities;
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
 
 namespace Microsoft.eShopWeb.ApplicationCore.Specifications
 {
 
     public class CatalogFilterSpecification : BaseSpecification<CatalogItem>
     {
-        public CatalogFilterSpecification(string searchText, int? brandId, int? typeId)
-            : base(catalogItem =>
+        /// <summary>
+        /// Build filter catalog expression
+        /// </summary>
+        /// <param name="searchText"></param>
+        /// <param name="brandId"></param>
+        /// <param name="typeId"></param>
+        /// <returns></returns>
+        public static Expression<Func<CatalogItem, bool>> BuildCatalogFilterExpression(
+            string searchText, int? brandId, int? typeId) {
+            return catalogItem =>
                 (!brandId.HasValue || catalogItem.CatalogBrandId == brandId) &&
                 (!typeId.HasValue || catalogItem.CatalogTypeId == typeId) &&
-                (string.IsNullOrEmpty(searchText) || catalogItem.Name.Contains(searchText)))
+                (string.IsNullOrEmpty(searchText) || catalogItem.Name.Contains(searchText, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public CatalogFilterSpecification(string searchText, int? brandId, int? typeId)
+            : base(BuildCatalogFilterExpression(searchText, brandId, typeId))
         {
         }
     }
