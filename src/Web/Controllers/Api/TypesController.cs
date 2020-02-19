ebc5f2fdd47cfb351ec531528142ed1221ced337
zcavaleiro <zcavaleiro@yahoo.com>
2020-02-19 12:30:56 +0000

    Added Logger information to Swagger Api

--- src/Web/Controllers/Api/TypesController.cs
+++ src/Web/Controllers/Api/TypesController.cs
@@ -7,6 +7,7 @@ using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
 using System.Linq;
 using System;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.eShopWeb.Web.Controllers.Api
 {
@@ -14,7 +15,13 @@ namespace Microsoft.eShopWeb.Web.Controllers.Api
     {
         private readonly IAsyncRepository<CatalogType> _typeRepository;
 
-        public TypesController(IAsyncRepository<CatalogType> typeRepository) => _typeRepository = typeRepository;
+        private readonly ILogger<TypesController> _logger;
+
+        public TypesController(IAsyncRepository<CatalogType> typeRepository, ILogger<TypesController> logger)
+        {
+            _typeRepository = typeRepository;
+            _logger = logger;
+        }
 
         /// <summary>
         /// return catalog types list
@@ -24,7 +31,11 @@ namespace Microsoft.eShopWeb.Web.Controllers.Api
         public async Task<ActionResult<IReadOnlyList<CatalogType>>> List()
         {
             var types = await _typeRepository.ListAllAsync();
+            _logger.LogInformation("################################");
+            _logger.LogInformation("Get all Catalog types");
+            _logger.LogInformation("################################");
             return Ok(types);
+
         }
 
         /// <summary>
@@ -34,11 +45,18 @@ namespace Microsoft.eShopWeb.Web.Controllers.Api
         /// <returns></returns>
         [HttpGet("{id}")]
         [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Get))]
-        public async Task<ActionResult<CatalogType>> GetById(int id) {
-            try  {
+        public async Task<ActionResult<CatalogType>> GetById(int id)
+        {
+            _logger.LogInformation("################################");
+            _logger.LogInformation("Get Catalog types by id");
+            _logger.LogInformation("################################");
+            try
+            {
                 var type = await _typeRepository.GetByIdAsync(id);
                 return Ok(type);
-            } catch (ModelNotFoundException) {
+            }
+            catch (ModelNotFoundException)
+            {
                 return NotFound();
             }
         }
@@ -50,12 +68,19 @@ namespace Microsoft.eShopWeb.Web.Controllers.Api
         /// <returns></returns>
         [HttpGet("{name}")]
         [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Get))]
-        public async Task<ActionResult<CatalogType>> GetByType(string name) {
-            try  {
+        public async Task<ActionResult<CatalogType>> GetByType(string name)
+        {
+            _logger.LogInformation("################################");
+            _logger.LogInformation("GET Catalog types by name");
+            _logger.LogInformation("################################");
+            try
+            {
                 var types = await _typeRepository.ListAllAsync();
                 var typeFindeds = types.Where(x => string.Compare(x.Type, name, StringComparison.InvariantCultureIgnoreCase) == 0);
                 return Ok(typeFindeds);
-            } catch (ModelNotFoundException) {
+            }
+            catch (ModelNotFoundException)
+            {
                 return NotFound();
             }
         }
@@ -69,16 +94,22 @@ namespace Microsoft.eShopWeb.Web.Controllers.Api
         [HttpPost]
         public async Task<ActionResult<IReadOnlyList<CatalogType>>> Post(CatalogType catalogType)
         {
-
-            try  {
+            _logger.LogInformation("################################");
+            _logger.LogInformation("POST Catalog types");
+            _logger.LogInformation("################################");
+            try
+            {
                 var types = await _typeRepository.ListAllAsync();
                 var typeFindeds = types
                         .Where(x => string.Compare(x.Type, catalogType.Type, StringComparison.InvariantCultureIgnoreCase) == 0);
-                if(typeFindeds != null){ return Conflict();}
+                if (typeFindeds != null) { return Conflict(); }
 
                 await _typeRepository.AddAsync(catalogType);
                 return Ok();
-            } catch (ModelNotFoundException) {
+            }
+            catch (ModelNotFoundException error)
+            {
+                _logger.LogError("O erro é: ", error);
                 return NotFound();
             }
         }
@@ -92,17 +123,24 @@ namespace Microsoft.eShopWeb.Web.Controllers.Api
         [HttpPut]
         public async Task<ActionResult<IReadOnlyList<CatalogType>>> Put(CatalogType catalogType)
         {
-            try  {
+            _logger.LogInformation("################################");
+            _logger.LogInformation("Update Catalog types");
+            _logger.LogInformation("################################");
+            try
+            {
                 var types = await _typeRepository.ListAllAsync();
                 // return catalog types list where de type name is the same and id not iqual
                 var typeFindeds = types
                         .Where(x => string.Compare(x.Type, catalogType.Type, StringComparison.InvariantCultureIgnoreCase) == 0)
                         .Where(x => x.Id != catalogType.Id);
-                if(typeFindeds != null){ return Conflict();}
+                if (typeFindeds != null) { return Conflict(); }
 
                 await _typeRepository.UpdateAsync(catalogType);
                 return Ok();
-            } catch (ModelNotFoundException) {
+            }
+            catch (ModelNotFoundException error)
+            {
+                _logger.LogError("O erro é: ", error);
                 return NotFound();
             }
         }
@@ -116,13 +154,20 @@ namespace Microsoft.eShopWeb.Web.Controllers.Api
         [HttpDelete("{id}")]
         public async Task<ActionResult<IReadOnlyList<CatalogType>>> Delete(int id)
         {
-            try  {
+            _logger.LogInformation("################################");
+            _logger.LogInformation("DELETE Catalog types");
+            _logger.LogInformation("################################");
+            try
+            {
                 var type = await _typeRepository.GetByIdAsync(id);
-                if(type == null){ return NotFound();}
+                if (type == null) { return NotFound(); }
 
                 await _typeRepository.DeleteAsync(type);
                 return Ok(type);
-            } catch (ModelNotFoundException) {
+            }
+            catch (ModelNotFoundException error)
+            {
+                _logger.LogError("O erro é: ", error);
                 return NotFound();
             }
         }
